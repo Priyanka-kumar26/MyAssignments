f831c2859a4951835cc7127e7849657b329c509c
Priyanka-kumar26 <priyankakumar545@gmail.com>
2026-07-08 10:06:11 +0530

    Add files via upload

---
+++ day1Assignment.js
@@ -0,0 +1,19 @@
+let userName="Raveen"
+console.log(userName)
+console.log(typeof userName)
+
+let Organisation="PWC"
+console.log(Organisation)
+console.log(typeof Organisation)
+
+let ContactNumber=1234567
+console.log(ContactNumber)
+console.log(typeof ContactNumber)
+
+let KnowsAutomation="true"
+console.log(KnowsAutomation)
+console.log(typeof KnowsAutomation)
+
+let usesPlaywright
+console.log(usesPlaywright)
+console.log(typeof usesPlaywright)
