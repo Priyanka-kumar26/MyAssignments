c98cf9afa8c3eb6fdfad4dd33b4d6a1c456191b1
Priyanka-kumar26 <priyankakumar545@gmail.com>
2026-07-09 10:27:54 +0530

    Add files via upload

---
+++ day3/conditionalStatements.js
@@ -0,0 +1,18 @@
+function isOddorEven(n) 
+{
+    
+    if (n/2==0) {
+        console.log("the number is even",n)
+
+    } 
+    else 
+        {
+        console.log("the number is odd",n);
+        
+        
+    }
+
+}
+    let n=10
+
+isOddorEven(n)
---
+++ day3/numberType.js
@@ -0,0 +1,16 @@
+function named(number) {
+  let result=""
+    if(number>0){
+       result="is Possitive"
+       
+    }
+    else if(number<0){
+       result="is negative"
+        
+    } else{
+result="is nuetral"        
+    }
+   return result
+}
+
+console.log(named(-4))
