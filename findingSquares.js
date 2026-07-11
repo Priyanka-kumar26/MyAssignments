8c23f0d6a4ecd9e5f691b96de1918cd0cdeb4e03
Priyanka-kumar26 <priyankakumar545@gmail.com>
2026-07-11 19:03:41 +0530

    Add files via upload

---
+++ findingSquares.js
@@ -0,0 +1,11 @@
+function findingSquare() {
+    for (let i = 10; i >0; i--) //intializing varible and decrementing by 1
+         {
+        let n=i*i  //arithmatic operation using multiplication
+        console.log("the square of the given no is ",n);
+        
+1        
+    }
+    
+}
+findingSquare()
